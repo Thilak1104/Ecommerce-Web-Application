3a94932c0b00bab3609823f23d4f24e2936826af
Thilak <thilaksaithipparapu6300@gmail.com>
2024-03-07 12:25:07 +0530

    Added context API and done with userAuthentication

---
+++ client/src/pages/Auth/Register.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import Layout from "../../components/Layout/Layout.js";
+import toast from "react-hot-toast"
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "../../styles/AuthStyles.css"
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  const navigate = useNavigate();
+
+  //form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("api/v1/auth/register", {
+        name,
+        email,
+        password,
+        phone,
+        address,
+      },{ validateStatus: false });
+      if (res && res.data.success) {
+        toast.success(res && res.data.message);
+        navigate("/login");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <Layout>
+      <div className="form-container">
+        <h1>REGISTER FORM</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="form-control"
+              id="exampleInputName1"
+              placeholder="Enter your name"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className="form-control"
+              id="exampleInputPhone1"
+              placeholder="Enter your mobile no"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="form-control"
+              id="exampleInputAddress1"
+              placeholder="Enter your address"
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            REGISTER
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Register;
