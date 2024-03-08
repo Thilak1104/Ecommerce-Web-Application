213928e6a4f4cd12dbaea6357670c354a6bec180
Thilak <thilaksaithipparapu6300@gmail.com>
2024-03-08 21:53:49 +0530

    Added crud for category in the client side

--- client/src/components/Layout/AdminMenu.js
+++ client/src/components/Layout/AdminMenu.js
@@ -1,32 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
 const AdminMenu = () => {
   return (
     
-    <div className="text-center">
-      <div className="list-group">
-        <h4>Admin Panel</h4>
-        <NavLink
-          to="/dashboard/admin/create-category"
-          class="list-group-item list-group-item-action"
-        >
-          Create Category
-        </NavLink>
-        <NavLink
-          to="/dashboard/admin/create-product"
-          class="list-group-item list-group-item-action"
-        >
-          Create Product
-        </NavLink>
-        <NavLink
-          to="/dashboard/admin/users"
-          class="list-group-item list-group-item-action"
-        >
-          Users
-        </NavLink>
+      <div className="text-center">
+        <div className="list-group">
+          <h4>Admin Panel</h4>
+          <NavLink
+            to="/dashboard/admin/create-category"
+            className="list-group-item list-group-item-action"
+          >
+            Create Category
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/create-product"
+            className="list-group-item list-group-item-action"
+          >
+            Create Product
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/users"
+            className="list-group-item list-group-item-action"
+          >
+            Users
+          </NavLink>
+        </div>
       </div>
-    </div>
+    
   );
 };
 
